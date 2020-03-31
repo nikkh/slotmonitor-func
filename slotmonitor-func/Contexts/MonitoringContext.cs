7e13aa82ac0197759d14ce48ad3f1c7184e2e18c
nhill <nhill@MININT-CU56C8E>
2020-03-31 10:30:57 +0100

    Add project files.

---
+++ slotmonitor-func/Contexts/MonitoringContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace slotmonitor_func.Contexts
+{
+    public class MonitoringContext
+    {
+        public string MonitoringContainerName { get; set; }
+        public string RequestHeaderFileName { get; set; }
+        public string RequestBodyFileName { get; set; }
+        public string NotifyUnavailability { get; set; }
+    }
+}
---
+++ slotmonitor-func/Startup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using slotmonitor_func.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace slotmonitor_func
+{
+    public class Startup : FunctionsStartup
+    {
+
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var localRoot = Environment.GetEnvironmentVariable("AzureWebJobsScriptRoot");
+            var azureRoot = $"{Environment.GetEnvironmentVariable("HOME")}/site/wwwroot";
+
+            var actualRoot = localRoot ?? azureRoot;
+
+            var configBuilder = new ConfigurationBuilder()
+                .SetBasePath(actualRoot)
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+            IConfiguration configuration = configBuilder.Build();
+            builder.Services.AddSingleton(configuration);
+            builder.Services.AddSingleton(configuration.GetSection("MonitoringContext").Get<MonitoringContext>());
+
+        }
+    }
+}
